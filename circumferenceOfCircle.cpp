a92a325aeebe7bdc7d3a151df94a441caadaad3b
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-09-29 00:23:31 +0000

    added code to circumferenceOfCircle.cpp

---
+++ circumferenceOfCircle.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2022 Kaitlyn Ip All rights reserved
+//
+// Created by: Kaitlyn Ip
+// Created on: Sep 2022
+// This program calculates the circumference of a circle
+//    with radius inputted from the user
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    // this function calculates the area and perimeter of a circle
+    const int radiusOfCircle;
+    double circumferenceOfCircle;
+
+    // input
+    std::cout << "Enter radius of the circle (mm): ";
+    std::cin >> radiusOfCircle;
+
+    // process
+    circumferenceOfCircle = radiusOfCircle * widthOfRectangle;
+    perimeterOfRectangle = 2 * (lengthOfRectangle + widthOfRectangle);
+
+    // output
+    std::cout << "" << std::endl;
+    std::cout << "Perimeter is " << (M_TAU * radiusOfCircle) << " mm." << std::endl;
+
+    std::cout << "\nDone." << std::endl;
+}
